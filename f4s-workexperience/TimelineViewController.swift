612dcb45b63fd9319afd3c8bb8411c585a54e041
Keith Staines <keith.staines@workfinder.com>
2018-07-01 06:11:04 +0100

    Ensured badge number is updated at the earliest opportunity

--- f4s-workexperience/TimelineViewController.swift
+++ f4s-workexperience/TimelineViewController.swift
@@ -52,6 +52,7 @@ class TimelineViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         getAllPlacementsForUser()
+        F4SUserStatusService.shared.beginStatusUpdate()
     }
     
     lazy var placementService: F4SPlacementService = {
