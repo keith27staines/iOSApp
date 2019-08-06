16f2407cd44e954df3caf8a99f18d31acc85313a
Keith Staines <keith.staines@workfinder.com>
2019-08-06 12:10:39 +0100

    Added tests  and documentation for F4SChoice

--- WorkfinderModules/WorkfinderCommon/WorkfinderCommon/Source/F4STemplateModels.swift
+++ WorkfinderModules/WorkfinderCommon/WorkfinderCommon/Source/F4STemplateModels.swift
@@ -73,15 +73,28 @@ extension F4STemplateBlank {
     }
 }
 
+/// An instance of F4SChoice represents a value selected by the YP to be inserted
+/// into a template blank, such as occurs in a covering letter template or email
+/// template
 public struct F4SChoice : Codable {
+    /// A UUID representing a specific value chosen by the YP to fill a blank in a template
+    /// (Note there is a "WTF" here - the UUID is a string representing a date when the blank being filled represents a date
     public var uuid: String
+    
+    /// The string to be filled into the template blank representing the YP's choice
     public var value: String
     
+    /// Initialises a new instance
+    ///
+    /// - Parameters:
+    ///   - uuid: Either a genuine UUID if the choice doesn't represent a date or a date string if it does
+    ///   - value: The string respresentation of the choice to be inserted into the template blank
     public init(uuid: String = "", value: String = "") {
         self.uuid = uuid
         self.value = value
     }
     
+    /// Returns true iff the uuid string is a date in Rfc3339 form
     public var uuidIsDate: Bool {
         return Date.dateFromRfc3339(string: uuid) == nil ? false : true
     }
