047501e72840b3d6a975daa43c5972128a8de64d
Keith Staines <keith.staines@workfinder.com>
2019-08-09 10:48:50 +0100

    added more test files

---
+++ WorkfinderModules/WorkfinderCommon/WorkfinderCommonTests/WorkfinderCommonStructsTests.swift
@@ -0,0 +1,16 @@
+//
+//  WorkfinderCommonStructsTests.swift
+//  WorkfinderCommonTests
+//
+//  Created by Keith Dev on 09/08/2019.
+//  Copyright © 2019 Founders4Schools. All rights reserved.
+//
+
+import XCTest
+import WorkfinderCommon
+
+class WorkfinderCommonStructsTests: XCTestCase {
+
+
+
+}
