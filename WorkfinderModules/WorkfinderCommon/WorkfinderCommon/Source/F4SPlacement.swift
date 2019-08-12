59fc46d70430449a334f03bf0582b75428a3db06
Keith Staines <keith.staines@workfinder.com>
2019-08-12 17:10:28 +0100

    added tests and documentation to F4SPlacement

--- WorkfinderModules/WorkfinderCommon/WorkfinderCommon/Source/F4SPlacement.swift
+++ WorkfinderModules/WorkfinderCommon/WorkfinderCommon/Source/F4SPlacement.swift
@@ -8,13 +8,29 @@
 
 import Foundation
 
+
+/// Models a work experience placement application
 public struct F4SPlacement : Codable {
+    /// company at which the placement takes place
     public var companyUuid: F4SUUID?
+    /// interests of the user
     public var interestList: [F4SInterest]
+    /// status of the application
     public var status: WEXPlacementState?
+    /// the unique id of the placement application
     public var placementUuid: F4SUUID?
+    /// the unique uuid of the user
     public var userUuid: F4SUUID?
     
+    
+    /// Initialise a new instance with the parameters provided
+    ///
+    /// - Parameters:
+    ///   - userUuid: the unique uuid of the user
+    ///   - companyUuid: the unique uuid of the company being applied to
+    ///   - interestList: a list of interests of the user
+    ///   - status: the status of the application
+    ///   - placementUuid: the unique uuid of the application
     public init(userUuid: F4SUUID? = nil, companyUuid: F4SUUID? = nil, interestList: [F4SInterest] = [], status: WEXPlacementState? = nil, placementUuid: F4SUUID? = nil) {
         self.userUuid = userUuid
         self.companyUuid = companyUuid
@@ -23,6 +39,10 @@ public struct F4SPlacement : Codable {
         self.placementUuid = placementUuid
     }
     
+    
+    /// Initialise a new instance from a timeline placement
+    ///
+    /// - Parameter timelinePlacement: the timeline placement
     public init(timelinePlacement: F4STimelinePlacement) {
         self.userUuid = timelinePlacement.userUuid
         self.companyUuid = timelinePlacement.companyUuid
---
+++ WorkfinderModules/WorkfinderCommon/WorkfinderCommonTests/F4SPlacementTests.swift
@@ -0,0 +1,57 @@
+//
+//  F4SPlacementTests.swift
+//  WorkfinderCommonTests
+//
+//  Created by Keith Dev on 12/08/2019.
+//  Copyright © 2019 Founders4Schools. All rights reserved.
+//
+
+import XCTest
+@testable import WorkfinderCommon
+
+class F4SPlacementTests: XCTestCase {
+
+    func test_initialise_with_timeline_placement() {
+        let timelinePlacement = F4STimelinePlacement(userUuid: "userUuid", companyUuid: "companyUuid", placementUuid: "placementUuid")
+        let sut = F4SPlacement(timelinePlacement: timelinePlacement)
+        XCTAssertEqual(sut.userUuid, "userUuid")
+        XCTAssertEqual(sut.companyUuid, "companyUuid")
+        XCTAssertEqual(sut.placementUuid, "placementUuid")
+        XCTAssertEqual(sut.status, timelinePlacement.workflowState)
+        XCTAssertTrue(sut.interestList.isEmpty)
+    }
+    
+    func test_initialise_with_values() {
+        let sut = F4SPlacement(userUuid: "userUuid", companyUuid: "companyUuid", interestList: [F4SInterest(id: 1, uuid: "interestUuid", name: "interestName")], status: WEXPlacementState.confirmed, placementUuid: "placementUuid")
+        XCTAssertEqual(sut.companyUuid, "companyUuid")
+        XCTAssertEqual(sut.placementUuid, "placementUuid")
+        XCTAssertEqual(sut.status, WEXPlacementState.confirmed)
+        XCTAssertEqual(sut.interestList.first?.uuid, "interestUuid")
+    }
+    
+    func test_decode() {
+        let json = """
+        {
+            \"user_uuid\": \"userUuid\",
+            \"company_uuid\": \"companyUuid\",
+            \"placement_uuid\": \"placementUuid\",
+            \"status\": \"confirmed\",
+            \"interests\": [
+                {
+                    \"id\": 1,
+                    \"uuid\": \"interestUuid\",
+                    \"name\": \"interestName\"
+                }
+            ]
+        }
+        """
+        let data = json.data(using: String.Encoding.utf8)!
+        let sut = try! JSONDecoder().decode(F4SPlacement.self, from: data)
+        XCTAssertEqual(sut.companyUuid, "companyUuid")
+        XCTAssertEqual(sut.userUuid, "userUuid")
+        XCTAssertEqual(sut.placementUuid, "placementUuid")
+        XCTAssertEqual(sut.status, WEXPlacementState.confirmed)
+        XCTAssertEqual(sut.interestList.first?.uuid, "interestUuid")
+    }
+
+}
