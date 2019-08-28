c19b319f8a6eafb3b72c27da5af42f8372535c80
Keith Staines <keith.staines@workfinder.com>
2019-08-28 14:54:30 +0100

    Added tests for apply and update

--- WorkfinderServices/WorkfinderServicesTests/F4SServicesTests/services inheriting from F4SDataTaskService/F4SPlacementApplicationServiceTests.swift
+++ WorkfinderServices/WorkfinderServicesTests/F4SServicesTests/services inheriting from F4SDataTaskService/F4SPlacementApplicationServiceTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import WorkfinderCommon
 @testable import WorkfinderServices
 
 class F4SPlacementApplicationServiceTests: XCTestCase {
@@ -8,6 +9,46 @@ class F4SPlacementApplicationServiceTests: XCTestCase {
         XCTAssertEqual(sut.apiName, "placement")
     }
     
+    func test_apply() {
+        let sut = F4SPlacementApplicationService()
+        let requiredValue = F4SPlacementJson(uuid: "uuid", user: "userUuid", company: "companyUuid", vendor: "vendorUuid", interests: nil)
+        let requiredResult = F4SNetworkResult.success(requiredValue)
+        let expectation  = XCTestExpectation(description: "")
+        sut.networkTaskfactory = MockF4SNetworkTaskFactory(requiredSuccessResult:
+            requiredResult)
+        let applyJson = F4SCreatePlacementJson(user: "userUuid", roleUuid: "roleUuid", company: "companyUuid", vendor: "vendorUuid", interests: [])
+        sut.apply(with: applyJson) { (result) in
+            switch result {
+            case .error(_):
+                XCTFail("This test was designed to return a success result")
+            case .success(let placement):
+                expectation.fulfill()
+                XCTAssertEqual(placement.userUuid,"userUuid")
+            }
+        }
+        wait(for: [expectation], timeout: 1)
+    }
     
+    func test_update() {
+        let sut = F4SPlacementApplicationService()
+        let requiredValue = F4SPlacementJson(uuid: "uuid", user: "userUuid", company: "companyUuid", vendor: "vendorUuid", interests: nil)
+        let requiredResult = F4SNetworkResult.success(requiredValue)
+        let expectation  = XCTestExpectation(description: "")
+        sut.networkTaskfactory = MockF4SNetworkTaskFactory(requiredSuccessResult:
+            requiredResult)
+        let patchJson = F4SPlacementJson(uuid: "placementUuid", user: "userUuid", company: "companyUuid", vendor: "vendorUuid", interests: nil)
+        sut.update(uuid: "placementUuid", with: patchJson) { (result) in
+            switch result {
+            case .error(_):
+                XCTFail("This test was designed to return a success result")
+            case .success(let placement):
+                expectation.fulfill()
+                XCTAssertEqual(placement.userUuid,"userUuid")
+            }
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
+
 
 }
