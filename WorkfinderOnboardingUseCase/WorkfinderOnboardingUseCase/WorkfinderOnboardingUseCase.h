e0c9159a5d5130db7dd596cfb84511b0fff1c9dd
Keith Staines <keith.staines@workfinder.com>
2019-09-13 13:23:38 +0100

    Setting up to factor out on boarding

---
+++ WorkfinderOnboardingUseCase/WorkfinderOnboardingUseCase/WorkfinderOnboardingUseCase.h
@@ -0,0 +1,19 @@
+//
+//  WorkfinderOnboardingUseCase.h
+//  WorkfinderOnboardingUseCase
+//
+//  Created by Keith Dev on 13/09/2019.
+//  Copyright © 2019 Founders4Schools. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkfinderOnboardingUseCase.
+FOUNDATION_EXPORT double WorkfinderOnboardingUseCaseVersionNumber;
+
+//! Project version string for WorkfinderOnboardingUseCase.
+FOUNDATION_EXPORT const unsigned char WorkfinderOnboardingUseCaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkfinderOnboardingUseCase/PublicHeader.h>
+
+
