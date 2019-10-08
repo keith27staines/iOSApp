b46feb98be6bbf31c64e1032fcf0fb50799b20f3
Keith Staines <keith.staines@workfinder.com>
2019-10-08 14:31:35 +0100

    completing reintegration of bugsnag directly without going through segment

--- Pods/Segment-Bugsnag/Pod/Classes/SEGBugsnagIntegrationFactory.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Analytics/SEGIntegrationFactory.h>
-
-
-@interface SEGBugsnagIntegrationFactory : NSObject <SEGIntegrationFactory>
-
-+ (instancetype)instance;
-
-@end
