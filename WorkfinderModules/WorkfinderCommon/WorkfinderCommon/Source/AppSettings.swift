f5ef54608ec267eaae974f459bdc13000862a2cf
Keith Staines <keith.staines@workfinder.com>
2020-01-13 13:04:11 +0000

    removed ab tests and all references to the remote configuration variables that controlled them

--- WorkfinderModules/WorkfinderCommon/WorkfinderCommon/Source/AppSettings.swift
+++ WorkfinderModules/WorkfinderCommon/WorkfinderCommon/Source/AppSettings.swift
@@ -10,13 +10,9 @@ public enum AppSettingKey: String, CaseIterable {
     // introduced in 2.9.0
     case displayName
     
-    // ab test introduced in 2.10
-    case ab_companyDetail_presentation_mode
-    
     public var defaultValue: String {
         switch self {
         case .displayName: return "Workfinder"
-        case .ab_companyDetail_presentation_mode: return "showTabs == true, first == company"
         }
     }
 
