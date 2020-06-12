3a851f08fcde860e520225e4cd7799dc13984fd4
Keith Staines <keith.staines@workfinder.com>
2020-06-12 16:54:36 +0100

    fixed  some missing links in setting the opt-ins on candidate creation

--- WorkfinderServices/WorkfinderServices/Source/User and candidate services/Candidate services.swift
+++ WorkfinderServices/WorkfinderServices/Source/User and candidate services/Candidate services.swift
@@ -13,19 +13,32 @@ public protocol UpdateCandidateServiceProtocol {
     func update(candidate: Candidate, completion: @escaping ((Result<Candidate, Error>) -> Void))
 }
 
+struct CreatableCandidate: Codable {
+    var date_of_birth: String?
+    var guardian_email: String?
+    var user: F4SUUID
+    var phone: String?
+    var has_allowed_sharing_with_educational_institution: Bool?
+    var has_allowed_sharing_with_employers: Bool?
+    
+    init(candidate: Candidate, userUuid: F4SUUID) {
+        user = userUuid
+        date_of_birth = candidate.dateOfBirth
+        guardian_email = candidate.guardianEmail
+        phone = candidate.phone
+        has_allowed_sharing_with_employers = candidate.allowedSharingWithEmployers
+        has_allowed_sharing_with_educational_institution = candidate.allowedSharingWithEducationInstitution
+    }
+}
+
 public class CreateCandidateService: WorkfinderService, CreateCandidateServiceProtocol {
     
     public func createCandidate(candidate: Candidate, userUuid: F4SUUID, completion: @escaping ((Result<Candidate, Error>) -> Void)) {
         
         do {
             let relativePath = "candidates/"
-            let jsonBody = [
-                "date_of_birth": candidate.dateOfBirth,
-                "guardian_email": candidate.guardianEmail,
-                "user": userUuid,
-                "phone": candidate.phone
-            ]
-            let request = try buildRequest(relativePath: relativePath, verb: .post, body: jsonBody)
+            let creatableCandidate = CreatableCandidate(candidate: candidate, userUuid: userUuid)
+            let request = try buildRequest(relativePath: relativePath, verb: .post, body: creatableCandidate)
             performTask(with: request, completion: completion, attempting: #function)
         } catch {
             completion(Result<Candidate,Error>.failure(error))
