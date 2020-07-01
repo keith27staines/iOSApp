769f816f7717e8f4161a4d53d9928a6f1c507407
Keith Staines <keith.staines@workfinder.com>
2020-07-01 07:55:10 +0100

    Create new framework for Recommendations tab

---
+++ WorkfinderRecommendationsList/WorkfinderRecommendationsList/WorkfinderRecommendationsList.h
@@ -0,0 +1,19 @@
+//
+//  WorkfinderRecommendationsList.h
+//  WorkfinderRecommendationsList
+//
+//  Created by Keith Dev on 01/07/2020.
+//  Copyright © 2020 Workfinder. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkfinderRecommendationsList.
+FOUNDATION_EXPORT double WorkfinderRecommendationsListVersionNumber;
+
+//! Project version string for WorkfinderRecommendationsList.
+FOUNDATION_EXPORT const unsigned char WorkfinderRecommendationsListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkfinderRecommendationsList/PublicHeader.h>
+
+
