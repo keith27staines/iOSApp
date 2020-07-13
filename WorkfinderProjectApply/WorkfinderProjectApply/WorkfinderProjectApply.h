0fdeb2e779022dcb891f8fb334e56ee9ceb5b3eb
Keith Staines <keith.staines@workfinder.com>
2020-07-13 12:48:47 +0100

    added WorkfinderProjectApply framework

---
+++ WorkfinderProjectApply/WorkfinderProjectApply/WorkfinderProjectApply.h
@@ -0,0 +1,19 @@
+//
+//  WorkfinderProjectApply.h
+//  WorkfinderProjectApply
+//
+//  Created by Keith Dev on 13/07/2020.
+//  Copyright © 2020 Workfinder. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkfinderProjectApply.
+FOUNDATION_EXPORT double WorkfinderProjectApplyVersionNumber;
+
+//! Project version string for WorkfinderProjectApply.
+FOUNDATION_EXPORT const unsigned char WorkfinderProjectApplyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkfinderProjectApply/PublicHeader.h>
+
+
