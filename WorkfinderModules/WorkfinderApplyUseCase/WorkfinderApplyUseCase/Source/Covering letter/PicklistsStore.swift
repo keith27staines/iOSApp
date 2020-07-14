e116834aa138317f4e5c6f0e86e13283baf48f94
Keith Staines <keith.staines@workfinder.com>
2020-07-14 17:34:57 +0100

    refactor

---
+++ WorkfinderModules/WorkfinderApplyUseCase/WorkfinderApplyUseCase/Source/Covering letter/PicklistsStore.swift
@@ -0,0 +1,80 @@
+
+import WorkfinderCommon
+
+public protocol PicklistsStoreProtocol {
+    var allPicklistsDictionary: PicklistsDictionary { get }
+    func load() -> PicklistsDictionary
+    func save()
+}
+public class PicklistsStore: PicklistsStoreProtocol {
+    let networkConfig: NetworkConfig
+    let localStore:LocalStorageProtocol
+    
+    public init(networkConfig: NetworkConfig, localStore:LocalStorageProtocol) {
+        self.networkConfig = networkConfig
+        self.localStore = localStore
+    }
+    
+    public var allPicklistsDictionary: PicklistsDictionary = [:]
+    
+    public func load() -> PicklistsDictionary {
+        let picklists = buildPicklists()
+        let selectedItems = loadSelectedItems()
+        assignSelectedValues(picklists: picklists, selectedItems: selectedItems)
+        allPicklistsDictionary = picklists
+        return picklists
+    }
+    
+    func loadSelectedItems() -> [PicklistType: [PicklistItemJson]] {
+        typealias ItemsDictionary = [PicklistType: [PicklistItemJson]]
+        guard
+            let data = localStore.value(key: LocalStore.Key.picklistsSelectedValuesData) as? Data,
+            let items = try? JSONDecoder().decode(ItemsDictionary.self, from: data)
+            else {
+            return [:]
+        }
+        return items
+    }
+    
+    public func save() {
+        var items = [PicklistType:[PicklistItemJson]]()
+        allPicklistsDictionary.forEach { (key, picklist) in
+            items[key] = picklist.selectedItems
+        }
+        let data = try? JSONEncoder().encode(items)
+        localStore.setValue(data, for: LocalStore.Key.picklistsSelectedValuesData)
+    }
+    
+    func assignSelectedValues(picklists: PicklistsDictionary,
+                              selectedItems: [PicklistType: [PicklistItemJson]]) {
+        picklists.forEach { (key, picklist) in
+            picklist.deselectAll()
+            picklist.selectItems(selectedItems[key] ?? [])
+        }
+    }
+    
+    func buildPicklists() -> PicklistsDictionary {
+        var lists = PicklistsDictionary()
+        PicklistType.allCases.forEach { (type) in
+            switch type {
+            case .year,
+                 .subject,
+                 .placementType,
+                 .project,
+                 .duration,
+                 .skills,
+                 .attributes:
+                lists[type] = Picklist(type: type, networkConfig: networkConfig)
+            case .institutions:
+                lists[type] = TextSearchPicklist(type: type, networkConfig: networkConfig)
+            case .motivation,
+                 .experience:
+                lists[type] = TextblockPicklist(type: type, networkConfig: networkConfig)
+            case .availabilityPeriod:
+                lists[type] = AvailabilityPeriodPicklist(networkConfig: networkConfig)
+            }
+        }
+        return lists
+    }
+}
+
