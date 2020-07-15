2f53af835435ae9cc5f11e5bf421e1df0c984ec7
Keith Staines <keith.staines@workfinder.com>
2020-07-15 04:53:55 +0100

    created skeleton cover letter framework

---
+++ WorkfinderCoverLetter/WorkfinderCoverLetter/WorkfinderCoverLetter.h
@@ -0,0 +1,19 @@
+//
+//  WorkfinderCoverLetter.h
+//  WorkfinderCoverLetter
+//
+//  Created by Keith Dev on 15/07/2020.
+//  Copyright © 2020 Workfinder. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkfinderCoverLetter.
+FOUNDATION_EXPORT double WorkfinderCoverLetterVersionNumber;
+
+//! Project version string for WorkfinderCoverLetter.
+FOUNDATION_EXPORT const unsigned char WorkfinderCoverLetterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkfinderCoverLetter/PublicHeader.h>
+
+
