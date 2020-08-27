5f68d3538ed5e4b61a93ec0373adb71e101d661d
Keith Staines <keith.staines@workfinder.com>
2020-08-27 13:24:34 +0100

    reworked recommendations list

---
+++ WorkfinderModules/WorkfinderCommon/WorkfinderCommon/Source/Recommendations/RecommendationsListItem.swift
@@ -0,0 +1,77 @@
+
+public struct RecommendationsListItem: Codable, Hashable {
+    public var uuid: F4SUUID?
+    public var user: F4SUUID?
+    public var associationUuid: F4SUUID?
+    public var createdAt: String?
+    public var sentAt: String?
+    public var confidence: Double?
+    public var project: Project?
+    
+    public init() {}
+    
+    enum CodingKeys: String, CodingKey {
+        case uuid
+        case user
+        case associationUuid = "association"
+        case project
+        case createdAt = "created_at"
+        case sentAt = "sent_at"
+        case confidence
+    }
+    
+    public struct Project: Codable, Equatable, Hashable {
+        public var uuid: F4SUUID?
+        public var isPaid: Bool?
+        public var isRemote: Bool?
+        public var duration: String?
+        public var type: ProjectType?
+        public var association: Association?
+        
+        enum CodingKeys: String, CodingKey {
+            case uuid
+            case isPaid = "is_paid"
+            case isRemote = "is_remote"
+            case duration
+            case type
+            case association
+        }
+        
+        public struct ProjectType: Codable, Equatable, Hashable {
+            public var uuid: F4SUUID?
+            public var name: String?
+        }
+        
+        public struct Association: Codable, Equatable, Hashable {
+            public var uuid: F4SUUID?
+            public var title: String?
+            public var host: Host?
+            public var location: Location?
+            
+            public struct Host: Codable, Equatable, Hashable {
+                public var uuid: F4SUUID?
+                public var photoUrl: String?
+                public var fullName: String?
+                enum CodingKeys: String, CodingKey {
+                    case uuid
+                    case fullName = "full_name"
+                }
+            }
+            
+            public struct Location: Codable, Equatable, Hashable {
+                public var company: Company?
+                public struct Company: Codable, Equatable, Hashable  {
+                    public var name: String?
+                    public var logo: String?
+                }
+            }
+        }
+        
+        public struct Activity: Codable, Equatable, Hashable {
+            public var uuid: F4SUUID?
+            public var name: String?
+        }
+    }
+    
+}
+
