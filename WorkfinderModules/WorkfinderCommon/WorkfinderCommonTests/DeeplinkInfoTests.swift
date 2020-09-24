0d9423fc59f7ff2b22ecf954061eae1234750cbd
Keith Staines <keith.staines@workfinder.com>
2020-09-24 13:48:04 +0100

    fixed unit tests

--- WorkfinderModules/WorkfinderCommon/WorkfinderCommonTests/DeeplinkInfoTests.swift
+++ WorkfinderModules/WorkfinderCommon/WorkfinderCommonTests/DeeplinkInfoTests.swift
@@ -5,11 +5,9 @@ import WorkfinderCommon
 class DeeplinkInfoTests: XCTestCase {
 
     func test_initialise() {
-        var sut = DeeplinkDispatchInfo(source: .deeplink, objectType: .application, action: .view(<#T##F4SUUID#>))
-        XCTAssertEqual(sut.objectId, "UUID")
+        var sut = DeeplinkDispatchInfo(source: .deeplink, objectType: .application, action: .view("UUID"))
         XCTAssertEqual(sut.objectType, .application)
         XCTAssertEqual(sut.source, .deeplink)
-        XCTAssertEqual(sut.action, .list)
     }
 
 }
