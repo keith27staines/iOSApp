be71c5cebda4e2ee9be3dc1ceb70b46f3f2c9daa
Keith Staines <keith.staines@workfinder.com>
2020-11-23 18:35:54 +0000

    adding datasource for tray

--- WorkfinderHome/WorkfinderHome/source/Home tab/Sub views/Discovery tray/DiscoveryTrayView.swift
+++ WorkfinderHome/WorkfinderHome/source/Home tab/Sub views/Discovery tray/DiscoveryTrayView.swift
@@ -3,27 +3,40 @@ import UIKit
 
 class DiscoveryTrayView : UIView {
 
-    var thumbButton: UIButton!
+    lazy var thumbButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "thumbScrollIndicator"), for: .normal)
+        addSubview(button)
+        button.anchor(top: topAnchor, leading: nil, bottom: nil, trailing: nil, padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0), size: CGSize(width: 44, height: 44))
+        button.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        return button
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.separatorStyle = .none
+        addSubview(tableView)
+        tableView.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+        return tableView
+    }()
     
     func refresh() {}
     
     func configureViews() {
-        configureThumbButton()
-    }
-    
-    init() {
-        super.init(frame: CGRect.zero)
+        isUserInteractionEnabled = true
         backgroundColor = UIColor.white
         layer.cornerRadius = 16
         layer.masksToBounds = false
         layer.shadowRadius = 2
         layer.shadowColor = UIColor.black.cgColor
         layer.shadowOpacity = 0.3
-        configureViews()
     }
     
-    @objc func handleSwipe(swipe: UISwipeGestureRecognizer) {
-
+    init() {
+        super.init(frame: CGRect.zero)
+        configureViews()
     }
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
@@ -32,14 +45,5 @@ class DiscoveryTrayView : UIView {
 
 extension DiscoveryTrayView {
     
-    func configureThumbButton() {
-        isUserInteractionEnabled = true
-        let button = UIButton()
-        //button.addTarget(self, action: #selector(changeDisplayState), for: .touchUpInside)
-        button.setImage(UIImage(named: "thumbScrollIndicator"), for: .normal)
-        addSubview(button)
-        button.anchor(top: topAnchor, leading: nil, bottom: nil, trailing: nil, padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0), size: CGSize(width: 44, height: 44))
-        button.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        thumbButton = button
-    }
+
 }
--- WorkfinderHome/WorkfinderHome/source/Home tab/Sub views/Discovery tray/DiscoveryViewController.swift
+++ WorkfinderHome/WorkfinderHome/source/Home tab/Sub views/Discovery tray/DiscoveryViewController.swift
@@ -2,13 +2,116 @@
 import UIKit
 import WorkfinderUI
 
-class DiscoveryTrayController {
+enum Section: Int, CaseIterable {
+    case searchBar
+    case popularOnWorkfinder
+    case recommendations
+    case topRoles
+    case recentRoles
+}
+
+class DiscoveryTrayController: NSObject {
     
-    let tray: DiscoveryTrayView
+    lazy var tray: DiscoveryTrayView = DiscoveryTrayView()
+    var tableView: UITableView { tray.tableView }
     
-    init() {
-        tray = DiscoveryTrayView()
+    override init() {
+        super.init()
+        configureTableView()
+    }
+    
+    func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(SearchFieldView.self, forCellReuseIdentifier: SearchFieldView.identifier)
+        tableView.register(PopularOnWorkfinderView.self, forCellReuseIdentifier: SearchFieldView.identifier)
+        tableView.register(RecommendationsView.self, forCellReuseIdentifier: SearchFieldView.identifier)
+        tableView.register(TopRolesView.self, forCellReuseIdentifier: SearchFieldView.identifier)
+        tableView.register(RecentRolesView.self, forCellReuseIdentifier: SearchFieldView.identifier)
+        tableView.register(SectionHeaderView.self, forHeaderFooterViewReuseIdentifier: SectionHeaderView.identifier)
+        tableView.register(SectionFooterView.self, forHeaderFooterViewReuseIdentifier: SectionFooterView.identifier)
+    }
+}
+
+extension DiscoveryTrayController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        Section.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let section = Section(rawValue: section) else { return 0 }
+        switch section {
+        case .searchBar:
+            return 1
+        case .popularOnWorkfinder:
+            return 1
+        case .recommendations:
+            return 1
+        case .topRoles:
+            return 1
+        case .recentRoles:
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let section = Section(rawValue: indexPath.section) else { return UITableViewCell() }
+        switch section {
+        case .searchBar:
+            return tableView.dequeueReusableCell(withIdentifier: SearchFieldView.identifier)
+        case .popularOnWorkfinder:
+            return 1
+        case .recommendations:
+            return 1
+        case .topRoles:
+            return 1
+        case .recentRoles:
+            return 1
+        }
     }
 
+}
+
+extension DiscoveryTrayController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
     
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        false
+    }
+    
+}
+
+class SearchFieldView: UITableViewCell {
+    static let identifier = "SearchFieldView"
+}
+
+class PopularOnWorkfinderView: UITableViewCell {
+    static let identifier = "PopularOnWorkfinderView"
+}
+
+class RecommendationsView: UITableViewCell {
+    static let identifier = "RecommendationsView"
+}
+
+class TopRolesView: UITableViewCell {
+    static let identifier = "TopRolesView"
+}
+
+class RecentRolesView: UITableViewCell {
+    static let identifier = "RecentRolesView"
+}
+
+class SectionHeaderView: UITableViewHeaderFooterView {
+    static let identifier = "SectionHeaderView"
+}
+
+class SectionFooterView: UITableViewHeaderFooterView {
+    static let identifier = "SectionFooterView"
 }
