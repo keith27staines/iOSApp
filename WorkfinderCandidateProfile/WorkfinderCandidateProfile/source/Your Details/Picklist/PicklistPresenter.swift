aafa979e14563262c90c6b4f0446b72ba41c1cdb
Keith Staines <keith.staines@workfinder.com>
2021-04-16 12:00:37 +0100

    factored out checkmark logic

--- WorkfinderCandidateProfile/WorkfinderCandidateProfile/source/Your Details/Picklist/PicklistPresenter.swift
+++ WorkfinderCandidateProfile/WorkfinderCandidateProfile/source/Your Details/Picklist/PicklistPresenter.swift
@@ -13,6 +13,7 @@ import WorkfinderServices
 class PicklistPresenter: BaseAccountPresenter {
 
     let picklist: AccountPicklist
+    var rowCheckManager: RowCheckManager?
     
     init(coordinator: AccountCoordinator, service: AccountServiceProtocol, picklist: AccountPicklist) {
         self.picklist = picklist
@@ -22,6 +23,7 @@ class PicklistPresenter: BaseAccountPresenter {
     var table: UITableView? {
         didSet {
             guard let table = table else { return }
+            rowCheckManager = RowCheckManager(table: table, picklist: picklist)
             let type = picklist.type
             let header = TableHeaderView(for: table, title: type.instruction, showSearchBar: type.showSearchBar)
             table.tableHeaderView = header
@@ -47,34 +49,7 @@ class PicklistPresenter: BaseAccountPresenter {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        guard let tappedId = picklist.itemForIndexPath(indexPath).id else { return }
-        let cell = tableView.cellForRow(at: indexPath)
-        let isCurrentlySelected = picklist.isItemSelectedAtIndexPath(indexPath)
-        if isCurrentlySelected {
-            if picklist.deselectItemWithId(tappedId) {
-                cell?.accessoryType = .none
-            }
-        } else {
-            if picklist.type.maxSelections == 1 {
-                if let currentSelectionId = picklist.firstSelectedItem()?.id {
-                    let didDeselect = picklist.deselectItemWithId(currentSelectionId)
-                    if didDeselect {
-                        if let currentSelectionIndexPath = picklist.indexPathForItem(with: currentSelectionId) {
-                            let cell = tableView.cellForRow(at: currentSelectionIndexPath)
-                            cell?.accessoryType = .none
-                        }
-                    }
-                }
-                if picklist.selectItemHavingId(tappedId) {
-                    cell?.accessoryType = .checkmark
-                }
-                
-            } else {
-                if picklist.selectItemHavingId(tappedId) {
-                    cell?.accessoryType = .checkmark
-                }
-            }
-        }
+        rowCheckManager?.onTap(indexPath: indexPath)
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -92,4 +67,59 @@ extension PicklistPresenter: UISearchBarDelegate {
         let indexSet = IndexSet(integersIn: 0..<picklist.sections.count)
         table?.reloadSections(indexSet, with: .none)
     }
+}
+
+
+/// RowCheckManager encapsulates the logic for checking and unchecking row in the table view
+class RowCheckManager {
+    
+    weak var table: UITableView?
+    weak var picklist: AccountPicklist?
+    
+    init(table: UITableView, picklist: AccountPicklist) {
+        self.table = table
+        self.picklist = picklist
+    }
+    
+    /// Checks, unchecks or moves the check according to whether the picklist allows multiple selections or not
+    func onTap(indexPath: IndexPath) {
+        guard
+            let table = table,
+            let picklist = picklist,
+            let tappedId = picklist.itemForIndexPath(indexPath).id,
+            let cell = table.cellForRow(at: indexPath)
+            else { return }
+        
+        
+        let isCurrentlySelected = picklist.isItemSelectedAtIndexPath(indexPath)
+        if isCurrentlySelected {
+            updateCell(cell, tappedId: tappedId, picklist: picklist, isChecked: false)
+        } else {
+            if picklist.type.maxSelections == 1,
+               let currentSelectionId = picklist.firstSelectedItem()?.id {
+                let didDeselect = picklist.deselectItemWithId(currentSelectionId)
+                if didDeselect {
+                    if let currentSelectionIndexPath = picklist.indexPathForItem(with: currentSelectionId) {
+                        let cell = table.cellForRow(at: currentSelectionIndexPath)
+                        cell?.accessoryType = .none
+                    }
+                }
+            }
+            updateCell(cell, tappedId: tappedId, picklist: picklist, isChecked: true)
+        }
+    }
+    
+    func updateCell(_ cell: UITableViewCell, tappedId: String, picklist: AccountPicklist, isChecked: Bool) {
+        switch isChecked {
+        case true:
+            if picklist.selectItemHavingId(tappedId) {
+                cell.accessoryType = .checkmark
+            }
+        case false:
+            if picklist.deselectItemWithId(tappedId) {
+                cell.accessoryType = .none
+            }
+        }
+    }
+    
 }
