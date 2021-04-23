426e591eeaaa6c80c2dcd982c550cf482a8c61d5
Keith Staines <keith.staines@workfinder.com>
2021-04-23 10:05:30 +0100

    Added framework for NPS

---
+++ WorkfinderNPS/WorkfinderNPS/WorkfinderNPS.h
@@ -0,0 +1,18 @@
+//
+//  WorkfinderNPS.h
+//  WorkfinderNPS
+//
+//  Created by Keith Staines on 23/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkfinderNPS.
+FOUNDATION_EXPORT double WorkfinderNPSVersionNumber;
+
+//! Project version string for WorkfinderNPS.
+FOUNDATION_EXPORT const unsigned char WorkfinderNPSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkfinderNPS/PublicHeader.h>
+
+
