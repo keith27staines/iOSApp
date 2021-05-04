a2829bd0ed8cd5ac0a125c5bf025d9c7182b7f6f
Keith <keith.staiines@workfinder.com>
2021-05-04 11:14:46 +0100

    fixed deer-linking

--- WorkfinderModules/WorkfinderCommon/WorkfinderCommon/DeeplinkInfo.swift
+++ WorkfinderModules/WorkfinderCommon/WorkfinderCommon/DeeplinkInfo.swift
@@ -1,5 +1,11 @@
 
 public struct DeeplinkRoutingInfo {
+    public let url: URL?
+    public let queryItems: [String: String]
+    public let source: Source
+    public let objectType: ObjectType
+    public let objectId: F4SUUID?
+    public var action: Action { objectId == nil ? .list : .open }
     
     public enum Source: String {
         case deeplink
@@ -30,83 +36,70 @@ public struct DeeplinkRoutingInfo {
         case open
     }
     
-    public var queryItems: [String: String]
-    public var source: Source
-    public var objectType: ObjectType
-    public var objectId: F4SUUID?
-    public var action: Action
-    
     public init(
         source: Source,
         objectType: ObjectType,
         objectId: F4SUUID?,
-        action: Action,
         queryItems: [String:String]) {
         self.source = source
         self.objectType = objectType
-        self.action = action
         self.objectId = objectId
         self.queryItems = queryItems
+        self.url = nil
     }
     
     public init?(pushNotification: PushNotification?) {
         guard
             let pushNotification = pushNotification,
-            let objectType = ObjectType(rawValue: pushNotification.objectType ?? ""),
-            let action = Action(rawValue: pushNotification.action ?? "")
+            let objectType = ObjectType(rawValue: pushNotification.objectType ?? "")
         else { return nil}
         
         self.init(
             source: Source.pushNotification,
             objectType: objectType,
             objectId: pushNotification.objectId,
-            action: action,
             queryItems: [:]
         )
     }
     
     public init?(deeplinkUrl: URL) {
-        guard
-            let (objectTypeString, objectId, queryItems) = DeeplinkRoutingInfo.deeplinkUrlToObjectAndId(url: deeplinkUrl),
-            let objectType: ObjectType = ObjectType(urlPathComponent: objectTypeString)
-        else { return nil }
-        let action: Action = (objectId == nil) ? Action.list : Action.open
+        self.source = .deeplink
+        self.url = deeplinkUrl
+        guard let components = URLComponents(url: deeplinkUrl, resolvingAgainstBaseURL: false) else {
+            return nil
+        }
+        let pathComponents = components.path.split(separator: "/")
+        switch components.scheme {
+        case "workfinderapp":
+            guard
+                let host = components.host,
+                let objectType = ObjectType(urlPathComponent: host)
+            else { return nil }
+            self.objectType = objectType
+            if let firstPathComponent = pathComponents.first {
+                self.objectId = String(firstPathComponent)
+            } else {
+                self.objectId = nil
+            }
+        default:
+            guard
+                let host = components.host,
+                host.contains("workfinder.com"),
+                let firstPathComponent = pathComponents.first,
+                let objectType = ObjectType(urlPathComponent: String(firstPathComponent))
+            else { return nil }
+            self.objectType = objectType
+            if pathComponents.count == 2 {
+                self.objectId = String(pathComponents[1])
+            } else {
+                self.objectId = nil
+            }
+        }
+        
         var parameters = [String:String]()
-        queryItems?.forEach {
+        components.queryItems?.forEach {
             parameters[$0.name] = $0.value
         }
-        self.init(
-            source: Source.deeplink,
-            objectType: objectType,
-            objectId: objectId,
-            action: action,
-            queryItems: parameters
-        )
-    }
-}
-
-extension DeeplinkRoutingInfo {
-    private static func deeplinkUrlToObjectAndId(url: URL) -> (String?, String?, [URLQueryItem]?)? {
-        if let placementUuid = placementViewRequestUuid(url: url) {
-            return ("placement", placementUuid, nil)
-        }
-        guard
-            let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
-            else { return nil }
-        var object = urlComponents.path
-        if object.first == "/" { object = String(object.dropFirst()) }
-        if object.last == "/" { object = String(object.dropLast())  }
-        return (urlComponents.host, object, urlComponents.queryItems)
-    }
-    
-    private static func placementViewRequestUuid(url: URL) -> F4SUUID? {
-        let prefix = "?placement="
-        guard
-            let path = url.absoluteString.removingPercentEncoding,
-            let index: String.Index = path.index(of: prefix)
-        else { return nil }
-        let offset = path.index(index, offsetBy: prefix.count)
-        let uuid = path[offset...]
-        return String(uuid)
+        self.queryItems = parameters
     }
 }
--- WorkfinderModules/WorkfinderCommon/WorkfinderCommonTests/DeeplinkInfoTests.swift
+++ WorkfinderModules/WorkfinderCommon/WorkfinderCommonTests/DeeplinkInfoTests.swift
@@ -5,14 +5,14 @@ import WorkfinderCommon
 class DeeplinkInfoTests: XCTestCase {
 
     func test_initialise() {
-        let sut = DeeplinkRoutingInfo(source: .deeplink, objectType: .placement, objectId: "1234", action: .open, queryItems: [:])
+        let sut = DeeplinkRoutingInfo(source: .deeplink, objectType: .placement, objectId: "1234", queryItems: [:])
         XCTAssertEqual(sut.objectType, .placement)
         XCTAssertEqual(sut.source, .deeplink)
         XCTAssertEqual(sut.objectId, "1234")
         XCTAssertEqual(sut.action, .open)
     }
     
-    func test_initialise_with_workfinderapp() {
+    func test_initialise_with_workfinderapp_with_uuid() {
         let url = URL(string: "workfinderapp://reviews/1234?query1=a&query2=b")!
         let sut = DeeplinkRoutingInfo(deeplinkUrl: url)
         XCTAssertEqual(sut?.objectType, .review)
@@ -20,12 +20,30 @@ class DeeplinkInfoTests: XCTestCase {
         XCTAssertEqual(sut?.queryItems["query1"], "a")
         XCTAssertEqual(sut?.queryItems["query2"], "b")
     }
-    
-    func test_initialise_with_workfinder.com() {
+
+    func test_initialise_with_workfinderapp_without_uuid() {
+        let url = URL(string: "workfinderapp://reviews/?query1=a&query2=b")!
+        let sut = DeeplinkRoutingInfo(deeplinkUrl: url)
+        XCTAssertEqual(sut?.objectType, .review)
+        XCTAssertNil(sut?.objectId)
+        XCTAssertEqual(sut?.queryItems["query1"], "a")
+        XCTAssertEqual(sut?.queryItems["query2"], "b")
+    }
+
+    func test_initialise_with_workfinder_with_uuid() {
         let url = URL(string: "https://develop.workfinder.com/reviews/1234?query1=a&query2=b")!
         let sut = DeeplinkRoutingInfo(deeplinkUrl: url)
         XCTAssertEqual(sut?.objectType, .review)
         XCTAssertEqual(sut?.objectId, "1234")
+        XCTAssertEqual(sut?.queryItems["query1"], "a")
+        XCTAssertEqual(sut?.queryItems["query2"], "b")
+    }
+
+    func test_initialise_with_workfinder_without_uuid() {
+        let url = URL(string: "https://develop.workfinder.com/reviews?query1=a&query2=b")!
+        let sut = DeeplinkRoutingInfo(deeplinkUrl: url)
+        XCTAssertEqual(sut?.objectType, .review)
+        XCTAssertNil(sut?.objectId)
         XCTAssertEqual(sut?.queryItems["query1"], "a")
         XCTAssertEqual(sut?.queryItems["query2"], "b")
     }
